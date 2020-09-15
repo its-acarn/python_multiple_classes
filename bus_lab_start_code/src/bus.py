caad72fbab7fda5d170c6fde36b542dd8fd08967
svvishjs <andreww92@live.com>
2020-09-15 15:16:33 +0100

    bus and person classes created

--- bus_lab_start_code/src/bus.py
+++ bus_lab_start_code/src/bus.py
@@ -1,1 +1,10 @@
+class Bus:
+
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+
+    def drive(self):
+        return "Brum brum"
+    
     
